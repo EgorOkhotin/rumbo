6f5e6af9ff68979440f849b3ee4c0486c4dbe939
ivlievAA <ivliev.aa@yandex.ru>
2023-07-28 19:35:31 +0300

    add toki for daemon main

--- rumbo_daemon/src/main.rs
+++ rumbo_daemon/src/main.rs
@@ -1,13 +1,10 @@
 mod config;
 use std::collections::BTreeMap;
-
 use crate::config::AppConfig;
 
-fn main() {
-    let config_property:String = config::read_config_file();
-    println!("111111111111");
-      println!("{}", config_property);
-    let properties:AppConfig=config::read_config(config_property).unwrap();
-    // print!(properties)
+#[tokio::main]
+async fn main() ->  std::io::Result<()>{
+
+  Ok(())
 }
 
