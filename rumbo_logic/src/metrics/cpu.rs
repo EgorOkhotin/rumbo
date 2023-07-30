d61f0f8bf839a450c52b34a1e6d4216cdd1b163a
Vladyslav Tkachenko <vladislavtkgum@gmail.com>
2023-07-30 22:24:29 +0300

    Delete debug timeout multiplier

--- rumbo_logic/src/metrics/cpu.rs
+++ rumbo_logic/src/metrics/cpu.rs
@@ -9,7 +9,7 @@ pub struct CpuUsageInfo {
 fn get_cpu_usage_info() -> Vec<CpuUsageInfo> {
     let mut system =
         System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
-    std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL * 5);
+    std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
     system.refresh_cpu();
     let mut i: u8 = 0;
     let cpu_usage = system
